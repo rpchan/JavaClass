b86927f7729bfa22098e5e96a549e105f443bd19
rpchan <74680701+rpchan@users.noreply.github.com>
2021-02-19 16:36:46 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
@@ -9,19 +10,19 @@ import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("MathCalculator.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
+	public void start(Stage stage) throws Exception {
+	      // loads Mortgage.fxml and configures the MortgageController
+	      Parent root = 
+	         FXMLLoader.load(getClass().getResource("Mortgage.fxml"));
+
+	      Scene scene = new Scene(root); // attach scene graph to scene
+	      stage.setTitle("Mortgage Calculator"); // displayed in window's title bar
+	      stage.setScene(scene); // attach scene to stage
+	      stage.show(); // display the stage
+	   }
+
 	public static void main(String[] args) {
 		launch(args);
 	}
 }
+
